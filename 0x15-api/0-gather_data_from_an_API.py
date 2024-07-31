520cb15ff8e440161f6d70626931c8a15d7e9a6e
Taurai Muzembe <tmuzembe@outlook.com>
2024-08-01 00:51:11 +0200

    challenge

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
     print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
                                                           len(completed),
                                                           len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+    [print("\t {}".format(title)) for title in completed]]
